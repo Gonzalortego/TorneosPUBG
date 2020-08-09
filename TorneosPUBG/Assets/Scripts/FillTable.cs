d00a1992196b3a4106c2400d7b466252db3fc800
Gonzalortego <57328653+Gonzalortego@users.noreply.github.com>
2020-08-08 23:50:10 -0300

    Tabla armada con contenido placeholder.

---
+++ TorneosPUBG/Assets/Scripts/FillTable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillTable : MonoBehaviour
+{
+    [SerializeField] private Transform idList;
+    [SerializeField] private Transform dateList;
+    [SerializeField] private GameObject prefabElement;
+    [SerializeField] private List<Color> colors;
+    [SerializeField] private int elementsCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        completeTableID();
+        completeTableDate();
+    }
+
+    private void completeTableID()
+    {
+        GameObject element;
+        for (int i = 0; i < elementsCount; i++)
+        {
+            element = Instantiate(prefabElement, idList);
+            element.GetComponent<Image>().color = colors[i % colors.Count];
+            element.GetComponentInChildren<Text>().text = i.ToString();
+        }
+    }
+
+    private void completeTableDate()
+    {
+        GameObject element;
+        for (int i = 0; i < elementsCount; i++)
+        {
+            element = Instantiate(prefabElement, dateList);
+            element.GetComponent<Image>().color = colors[i % colors.Count];
+            element.GetComponentInChildren<Text>().text = i.ToString();
+        }
+    }
+
+}
